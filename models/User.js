80d11e42a0106cc88667a5b895a41eb5828a10e4
vanibannuru <vani.b@waycool.in>
2022-09-05 10:29:31 +0530

    mongo url added in configenv file

--- models/User.js
+++ models/User.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Please provide your name'],
+    required: [true, "Please provide your name"],
   },
   email: {
     type: String,
-    required: [true, 'Please provide your email'],
+    required: [true, "Please provide your email"],
     unique: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email',
+      "Please add a valid email",
     ],
   },
   password: {
     type: String,
-    required: [true, 'Password is required'],
+    required: [true, "Password is required"],
     minlength: 6,
     select: false,
   },
@@ -30,10 +30,10 @@ const UserSchema = mongoose.Schema({
 
 //#region ~ Encrypt Password Before Saveing  :-}
 // Using function key-word insted of arrow function to use "this" to get Users properties
-UserSchema.pre('save', async function (next) {
+UserSchema.pre("save", async function (next) {
   // The below step usefull in update if password has modified then bcrypt it or
   // leave it if password has not changed
-  if (!this.isModified('password')) next();
+  if (!this.isModified("password")) next();
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
@@ -50,11 +50,9 @@ UserSchema.methods.getSignedJwtToken = function () {
 //#endregion
 
 //#region ~ Match Password :-}
-
 UserSchema.methods.matchPassword = async function (enterdPassword) {
   return await bcrypt.compare(enterdPassword, this.password);
 };
-
 //#endregion
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
